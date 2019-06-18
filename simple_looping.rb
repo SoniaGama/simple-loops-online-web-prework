5f14d02d522497750f7a6a4fb608562cbe288315
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 19:27:27 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -101,9 +101,10 @@ end
 
 #Example for
 
-#for counter in 1..40 do
-#  puts "The current number is #{counter}."
-#end
+shoe_count = 1..32
+for shoe in shoe_count 
+    puts "I'm putting a pair of shoes away."
+end
 
 
 
