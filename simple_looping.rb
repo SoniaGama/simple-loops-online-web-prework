a041e575d8c586591f5e3aed65a7e8adff03c7dd
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-18 19:28:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -93,9 +93,8 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  range = 1..
-  
-  for counter in number_of_times do
+  range = 1..number_of_times
+  for counter in range do
     puts phrase
   end
   
